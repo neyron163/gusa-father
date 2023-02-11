a0d2d0444c77ef60621c79c45c91819d66a2b44c
Maxim Sedelnikov <max.aleksandrovich163@gmail.com>
2023-02-11 13:11:44 +0400

    used conversation id to extend api context

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const BOT_TOKEN = process.env.BOT_TOKEN;
+export const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,33 @@
 import { ChatGPTAPI } from 'chatgpt';
-import * as dotenv from 'dotenv';
 import { Telegraf } from 'telegraf';
 
-dotenv.config();
+import { BOT_TOKEN, OPENAI_API_KEY } from './constants';
+import { DATA } from './data';
 
-if (!process.env.BOT_TOKEN || !process.env.OPENAI_API_KEY) {
+if (!BOT_TOKEN || !OPENAI_API_KEY) {
     throw new Error('Bot token or chatgpt doesn`t exists');
 }
 
 const api = new ChatGPTAPI({
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey: OPENAI_API_KEY,
 });
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new Telegraf(BOT_TOKEN);
 
 bot.command('chatbot', async (ctx) => {
     const messageFromGroup = ctx.message.text.replace('/chatbot', '');
+    const currentUserId = ctx.from.id;
+    const currentUseData = DATA.users[currentUserId];
 
-    const answerFromGpt = await api.sendMessage(messageFromGroup);
+    const answerFromGpt = await api.sendMessage(
+        messageFromGroup,
+        currentUseData && {
+            conversationId: DATA.users[currentUserId].cId,
+            parentMessageId: DATA.users[currentUserId].pId,
+        },
+    );
+
+    DATA.users = { ...DATA.users, [ctx.from.id]: { cId: answerFromGpt.conversationId, pId: answerFromGpt.id } };
 
     ctx.reply(answerFromGpt);
 });
